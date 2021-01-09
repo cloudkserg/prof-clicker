768c0eaf6d6871ef47477446987f4fbad263567f
Mikhail Pimenov <tomorrowind@ya.ru>
2021-01-09 17:19:31 +0300

    feat: factory view

---
+++ src/Factory.js
@@ -0,0 +1,9 @@
+export default function Factory(props) {
+    return (
+        <div className="factory-container">
+            <div className="factory"></div>
+            <div className="road"></div>
+            <div className="bus"></div>
+        </div>
+    );
+}
